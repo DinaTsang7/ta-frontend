30430a57ea3c61c089ed03e9cf704f10f42aa53b
Yuchao Chen <yuchaoc@student.unimelb.edu.au>
2017-08-01 19:20:15 +1000

    modify login page UI

--- login.js
+++ login.js
@@ -1,47 +1,57 @@
-            function login(googleUser) {
-           // var profile = googleUser.getBasicProfile();
-            
-            // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-            // console.log('Name: ' + profile.getName());
-            // console.log('Image URL: ' + profile.getImageUrl());
-            // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-        
-            var profile = googleUser.getBasicProfile()
-            var id_token = googleUser.getAuthResponse().id_token
-                console.log(id_token) 
-                $.ajax({
-                type: 'POST',
-                url: "/auth/testLogin",
-                data: {
-                    accessToken: id_token
-                },
-                dataType: 'json',
-                  statusCode: {
-            404: function() {
-              alert( "找不到页面" );
-            },
-            200: function(result){
-              // console.log(result);
-            },
-            500: function(result){
-              alert("500");
-            },
-            401: function(result){
-              alert("401");
-            }
-          }
-                // success: function( result ) {
-                //     console.log(result)
-                //     //alert(result);
-                //     //$( "#weather-temp" ).html( "<strong>" + result + "</strong> degrees" );
-                // }
-                });
-                
-            }
-            
-        // $(function(){
+function login(googleUser) {
+    // var profile = googleUser.getBasicProfile();
 
-        // })
+    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    // console.log('Name: ' + profile.getName());
+    // console.log('Image URL: ' + profile.getImageUrl());
+    // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+    var profile = googleUser.getBasicProfile()
+    var id_token = googleUser.getAuthResponse().id_token
+    console.log(id_token)
+    $.ajax({
+        type: 'POST',
+        url: "/auth/testLogin",
+        data: {
+            accessToken: id_token
+        },
+        dataType: 'json',
+        statusCode: {
+            404: function () {
+                
+                alert("找不到页面");
+            },
+            200: function (result) {
+                // console.log(result);
+            },
+            500: function (result) {
+                signOut();
+                alert("Signed out since the account is not recorded in the system");
+            },
+            401: function (result) {
+                signOut();
+                alert("401");
+            }
+        }
+        // success: function( result ) {
+        //     console.log(result)
+        //     //alert(result);
+        //     //$( "#weather-temp" ).html( "<strong>" + result + "</strong> degrees" );
+        // }
+    });
+
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
+// $(function(){
+
+// })
 
 
 // function login() {
